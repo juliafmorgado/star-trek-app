65704a97fa64795e5d32d75ebc54d03f5b73ef7f
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-02 16:29:30 -0400

    created variable for input

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
  document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    
+    const alienName = document.querySelector('input').value.toLocaleLowerCase()
+
 }
