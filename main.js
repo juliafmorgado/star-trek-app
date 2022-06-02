0779278fbf32e802b8924b2b029f096ccf7c17a5
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-02 16:32:28 -0400

    fixed typo

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 async function apiRequest(){
     const alienName = document.querySelector('input').value.toLocaleLowerCase()
     try{
-        const response = await fetch(`https://st-api-demo.herokuapp.com/?${alienName}`)
+        const response = await fetch(`https://st-api-demo.herokuapp.com/${alienName}`)
     } catch{
 
     }
