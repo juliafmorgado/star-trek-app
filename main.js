ae7834e3658464df6af46871410a3093cf5eb2ab
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-02 16:31:41 -0400

    created fetch api and await

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 async function apiRequest(){
     const alienName = document.querySelector('input').value.toLocaleLowerCase()
     try{
-
+        const response = await fetch(`https://st-api-demo.herokuapp.com/?${alienName}`)
     } catch{
-        
+
     }
 }
