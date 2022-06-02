79640a65c68955f00f5fcf5e58214ec466428a6a
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-06-02 16:52:28 -0400

    added css

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
  document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const alienName = document.querySelector('input').value.toLocaleLowerCase()
+    const alienName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://st-api-demo.herokuapp.com/${alienName}`)
+        const response = await fetch(`https://st-api-demo.herokuapp.com/api/${alienName}`)
         const data = await response.json()
         console.log(data);
     } catch(error){
